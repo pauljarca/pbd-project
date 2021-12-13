958a57397c17c066f0555195b08360f98006f446
Paul <paulvjarca@gmail.com>
2021-12-13 23:29:07 +0200

    add table to functions and users

--- WebProjectPBD/WebProjectPBD/Functions.aspx.designer.cs
+++ WebProjectPBD/WebProjectPBD/Functions.aspx.designer.cs
@@ -15,21 +15,39 @@ namespace WebProjectPBD
     {
 
         /// <summary>
-        /// Button1 control.
+        /// GridView1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.GridView GridView1;
 
         /// <summary>
-        /// Button2 control.
+        /// SqlDataSource1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+
+        /// <summary>
+        /// GridView2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView2;
+
+        /// <summary>
+        /// SqlDataSource2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
     }
 }
--- WebProjectPBD/WebProjectPBD/Sah.aspx
+++ WebProjectPBD/WebProjectPBD/Sah.aspx
@@ -10,17 +10,6 @@
             </ul>
         </header>
     </div>
-     <div class="row justify-content-center">
-        <div class="card" style="width: 18rem; margin: 10px;">
-            <img src="https://images.unsplash.com/photo-1551198581-aec5c1556d7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Table</h5>
-                <p class="card-text">Înregistreză următorul joc de table.</p>
-            </div>
-        </div>
-       </div>
-
-
     <div class="container">
 
 
