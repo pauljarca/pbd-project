92a7e24f8bc2905514ffe4e0582e5f441615134d
Boran Rusalin Lucian <alinboarn2@gmail.com>
2021-12-18 03:08:03 +0200

    sorting

---
+++ WebProjectPBD/WebProjectPBD/Functions3.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Functions3.aspx.cs" Inherits="WebProjectPBD.Functions3" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+         <div class="container">
+        <header class="d-flex justify-content-center py-3">
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a href="Homepage.aspx" class="nav-link">Overview</a></li>
+                <li class="nav-item"><a href="Functions3.aspx" class="nav-link active" aria-current="page">Functions</a></li>
+            </ul>
+        </header> 
+             <div class="container justify-content-center">
+                  <h2>Date jucători
+                   </h2>
+                 <asp:GridView ID="GridView1" runat="server">
+                     </asp:GridView>
+             </div>
+    </div>
+       <div class="container">
+        <h2>Date jocuri
+           </h2>
+           <asp:GridView ID="GridView2" runat="server">
+            </asp:GridView>
+       
+    </div>
+    <div class="container">
+        <h2>Optiuni</h2>
+        <div class="row">
+            <asp:Button ID="Button2" runat="server" CssClass="btn btn-primary" Text="Sortare crescator" OnClick="Button2_Click" />
+             <asp:Button ID="Button3" runat="server" CssClass="btn btn-primary" Text="Afisare conditionata" OnClick="Button3_Click" />
+        </div>
+    </div>
+</asp:Content>
---
+++ WebProjectPBD/WebProjectPBD/Functions3.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProjectPBD
+{
+    public partial class Functions3 : System.Web.UI.Page
+    {
+        static string connString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\FAC\pbd\cod\pbd-project\WebProjectPBD\WebProjectPBD\App_Data\db.mdf;Integrated Security=True";
+        SqlConnection sqlConn = new SqlConnection(connString);
+        SqlCommand cmd;
+        DataTable dt = new DataTable();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            sqlConn.Open();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT  * FROM Jucatori", sqlConn);
+            dt = new DataTable();
+            sqlDa.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+            SqlDataAdapter sqlDa1 = new SqlDataAdapter("SELECT * FROM Joc", sqlConn);
+            dt = new DataTable();
+            sqlDa1.Fill(dt);
+            GridView2.DataSource = dt;
+            GridView2.DataBind();
+
+            sqlConn.Close();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            sqlConn.Open();
+            SqlDataAdapter sqlDA2 = new SqlDataAdapter("SELECT * FROM Joc ORDER BY Data_inceput_joc ASC",sqlConn);
+            dt = new DataTable();
+            sqlDA2.Fill(dt);
+            GridView2.DataSource = dt;
+            GridView2.DataBind();
+            sqlConn.Close();
+
+
+        }
+
+        
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            sqlConn.Open();
+            SqlDataAdapter sqlDA4 = new SqlDataAdapter("SELECT Id_joc,Tip_joc,Partida_totala,Data_inceput_joc,Data_sfarsit_joc,DATEDIFF(hour,Data_inceput_joc,Data_sfarsit_joc) AS Durata_joc FROM Joc WHERE Data_sfarsit_joc BETWEEN '01/01/2010' AND '04/01/2010' ORDER BY Tip_joc,Data_inceput_joc ASC", sqlConn);
+            dt = new DataTable();
+
+            sqlDA4.Fill(dt);
+            GridView2.DataSource = dt;
+            GridView2.DataBind();
+
+            sqlConn.Close();
+
+        }
+       
+
+
+    }
+}
