d76f13738d52bcbf8706cacc778f74865e152f5d
Boran Rusalin Lucian <alinboarn2@gmail.com>
2021-12-19 16:20:22 +0200

    add data and id

--- WebProjectPBD/WebProjectPBD/JocActiv.aspx
+++ WebProjectPBD/WebProjectPBD/JocActiv.aspx
@@ -6,7 +6,7 @@
         <header class="d-flex justify-content-center py-3">
             <ul class="nav nav-pills">
                 <li class="nav-item"><a href="Homepage.aspx" class="nav-link active" aria-current="page">Overview</a></li>
-                <li class="nav-item"><a href="Functions.aspx" class="nav-link">Functions</a></li>
+                <li class="nav-item"><a href="Functions3.aspx" class="nav-link">Functions</a></li>
             </ul>
         </header>
     </div>
@@ -24,7 +24,9 @@
         <div class="card" style="width: 18rem; margin: 10px;">
             <div class="card-body">
                 <h5 class="card-title"><asp:Label ID="Label1" runat="server" Text="Scor Jucator 1"></asp:Label></h5>
-                <p class="card-text"> <asp:Label ID="ScorJucator1_lb" runat="server" Text="0"></asp:Label></p>
+                <p class="card-text"> 
+                    <asp:Label ID="ScorJucator1_lb" runat="server" Text="0"></asp:Label>
+                </p>
                <asp:Button ID="Jucator1_btn" runat="server" Text="Jucator 1"  class="btn btn-primary" OnClick="Jucator1_btn_Click" />
             </div>
         </div>
@@ -34,7 +36,9 @@
         <div class="card" style="width: 18rem; margin: 10px;">
             <div class="card-body">
                 <h5 class="card-title"><asp:Label ID="Label2" runat="server" Text="Scor Jucator 2"></asp:Label></h5>
-                <p class="card-text"><asp:Label ID="ScorJucator2_lb" runat="server" Text="0"></asp:Label></p>
+                <p class="card-text">
+                    <asp:Label ID="ScorJucator2_lb" runat="server" Text="0"></asp:Label>
+                </p>
                  <asp:Button ID="Jucator2_btn" runat="server" Text="Jucator2" class="btn btn-primary" OnClick="Jucator2_btn_Click" />
             </div>
         </div>
--- WebProjectPBD/WebProjectPBD/JocActiv.aspx.cs
+++ WebProjectPBD/WebProjectPBD/JocActiv.aspx.cs
@@ -11,16 +11,19 @@ namespace WebProjectPBD
 {
     public partial class JocActiv : System.Web.UI.Page
     {
-        static string connString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Info An 3\PBD\pbd-project\WebProjectPBD\WebProjectPBD\App_Data\db.mdf;Integrated Security=True";
+        static string connString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\FAC\pbd\cod\pbd-project\WebProjectPBD\WebProjectPBD\App_Data\db.mdf;Integrated Security=True";
         SqlConnection sqlConn = new SqlConnection(connString);
         SqlCommand cmd;
         SqlCommand cmd2;
+        SqlCommand cmd3;
+        SqlCommand cmd4;
 
         private int id_joc;
         private int n_partida;
         private int n_partida_curenta;
         private int n_scor1;
         private int n_scor2;
+        private int id_invingator;
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -43,7 +46,7 @@ namespace WebProjectPBD
             else Lbl_remaining_rounds.Text = "Joc table";
             Label1.Text = reader["Jucator1"].ToString();
             Label2.Text = reader["Jucator2"].ToString();
-            ScorJucator1_lb.Text = n_scor2.ToString();
+            ScorJucator1_lb.Text = n_scor1.ToString();
             ScorJucator2_lb.Text = n_scor2.ToString();
             reader.Close();
         }
@@ -54,7 +57,7 @@ namespace WebProjectPBD
         {
             n_scor1 += 1;
             n_partida_curenta += 1;
-            cmd2 = new SqlCommand("UPDATE Joc SET Partida_curenta=@partida, Scor_jucator1=@scor1 WHERE Id_joc=@id;", sqlConn);
+            cmd2 = new SqlCommand("UPDATE Joc SET Partida_curenta=@partida, Scor_jucator1=@scor1 WHERE Id_joc=@id", sqlConn);
             cmd2.Parameters.AddWithValue("@partida", n_partida_curenta.ToString());
             cmd2.Parameters.AddWithValue("@scor1", n_scor1.ToString());
             cmd2.Parameters.AddWithValue("@id", id_joc.ToString());
@@ -63,13 +66,53 @@ namespace WebProjectPBD
             ScorJucator1_lb.Text = n_scor1.ToString();
             if (n_partida_curenta >= n_partida)
             {
+
+               
+                
+
                 winner_lbl.Visible = true;
                 Button1.Visible = true;
                 Jucator1_btn.Visible = false;
                 Jucator2_btn.Visible = false;
-                winner_lbl.Text = n_scor1 > n_scor2
-                    ? "Jocul s-a terminat! " + Convert.ToString(Label1.Text) + " a castigat!"
-                    : "Jocul s-a terminat! " + Convert.ToString(Label2.Text) + " a castigat!";
+
+                if (n_scor1 > n_scor2)
+                {
+                    cmd4 = new SqlCommand("SELECT * FROM Jucatori WHERE Nume=@nume", sqlConn);
+                    SqlDataReader reader;
+                    cmd4.Parameters.Add("@nume", Label1.Text);
+                    reader = cmd4.ExecuteReader();
+                    reader.Read();
+                    id_invingator = Int32.Parse(reader["Id"].ToString());
+                    reader.Close();
+                    cmd4.ExecuteNonQuery();
+
+
+                    cmd2 = new SqlCommand("UPDATE Joc SET Data_sfarsit_joc=@data_s,Id_invingator=@id_i WHERE Id_joc=@id ", sqlConn);
+                    cmd2.Parameters.AddWithValue("@data_s", DateTime.Now);
+                    cmd2.Parameters.AddWithValue("@id_i", id_invingator.ToString());
+                    cmd2.Parameters.AddWithValue("@id", id_joc.ToString());
+                    cmd2.ExecuteNonQuery();
+                    winner_lbl.Text = "Jocul s-a terminat! " + Convert.ToString(Label1.Text) + " a castigat!";
+                }
+                else
+                {
+                    cmd4 = new SqlCommand("SELECT * FROM Jucatori WHERE Nume=@nume", sqlConn);
+                    SqlDataReader reader;
+                    cmd4.Parameters.Add("@nume", Label2.Text);
+                    reader = cmd4.ExecuteReader();
+                    reader.Read();
+                    id_invingator = Int32.Parse(reader["Id"].ToString());
+                    reader.Close();
+                    cmd4.ExecuteNonQuery();
+
+
+                    cmd2 = new SqlCommand("UPDATE Joc SET Data_sfarsit_joc=@data_s,Id_invingator=@id_i WHERE Id_joc=@id ", sqlConn);
+                    cmd2.Parameters.AddWithValue("@data_s", DateTime.Now);
+                    cmd2.Parameters.AddWithValue("@id_i", id_invingator.ToString());
+                    cmd2.Parameters.AddWithValue("@id", id_joc.ToString());
+                    cmd2.ExecuteNonQuery();
+                    winner_lbl.Text = "Jocul s-a terminat! " + Convert.ToString(Label2.Text) + " a castigat!";
+                }
             }
         }
 
@@ -82,11 +125,11 @@ namespace WebProjectPBD
         {
             n_scor2 += 1;
             n_partida_curenta += 1;
-            cmd2 = new SqlCommand("UPDATE Joc SET Partida_curenta=@partida, Scor_jucator2=@scor2 WHERE Id_joc=@id;", sqlConn);
-            cmd2.Parameters.AddWithValue("@partida", n_partida_curenta.ToString());
-            cmd2.Parameters.AddWithValue("@scor2", n_scor2.ToString());
-            cmd2.Parameters.AddWithValue("@id", id_joc.ToString());
-            cmd2.ExecuteNonQuery();
+            cmd3 = new SqlCommand("UPDATE Joc SET Partida_curenta=@partida, Scor_jucator2=@scor2 WHERE Id_joc=@id;", sqlConn);
+            cmd3.Parameters.AddWithValue("@partida", n_partida_curenta.ToString());
+            cmd3.Parameters.AddWithValue("@scor2", n_scor2.ToString());
+            cmd3.Parameters.AddWithValue("@id", id_joc.ToString());
+            cmd3.ExecuteNonQuery();
             Lbl_round.Text = "Partida " + n_partida_curenta.ToString() + " din " + n_partida.ToString();
             ScorJucator2_lb.Text = n_scor2.ToString();
             if (n_partida_curenta >= n_partida)
@@ -95,9 +138,45 @@ namespace WebProjectPBD
                 Button1.Visible = true;
                 Jucator1_btn.Visible = false;
                 Jucator2_btn.Visible = false;
-                winner_lbl.Text = n_scor1 > n_scor2
-                    ? "Jocul s-a terminat! " + Convert.ToString(Label1.Text) + " a castigat!"
-                    : "Jocul s-a terminat! " + Convert.ToString(Label2.Text) + " a castigat!";
+                if (n_scor2 > n_scor1)
+                {
+                    cmd4 = new SqlCommand("SELECT * FROM Jucatori WHERE Nume=@nume", sqlConn);
+                    SqlDataReader reader;
+                    cmd4.Parameters.Add("@nume", Label2.Text);
+                    reader = cmd4.ExecuteReader();
+                    reader.Read();
+                    id_invingator = Int32.Parse(reader["Id"].ToString());
+                    reader.Close();
+                    cmd4.ExecuteNonQuery();
+
+
+                    cmd2 = new SqlCommand("UPDATE Joc SET Data_sfarsit_joc=@data_s,Id_invingator=@id_i WHERE Id_joc=@id ", sqlConn);
+                    cmd2.Parameters.AddWithValue("@data_s", DateTime.Now);
+                    cmd2.Parameters.AddWithValue("@id_i", id_invingator.ToString());
+                    cmd2.Parameters.AddWithValue("@id", id_joc.ToString());
+                    cmd2.ExecuteNonQuery();
+                    winner_lbl.Text = "Jocul s-a terminat! " + Convert.ToString(Label2.Text) + " a castigat!";
+                }
+                else
+                {
+                    cmd4 = new SqlCommand("SELECT * FROM Jucatori WHERE Nume=@nume", sqlConn);
+                    SqlDataReader reader;
+                    cmd4.Parameters.Add("@nume", Label1.Text);
+                    reader = cmd4.ExecuteReader();
+                    reader.Read();
+                    id_invingator = Int32.Parse(reader["Id"].ToString());
+                    reader.Close();
+                    cmd4.ExecuteNonQuery();
+
+
+                    cmd2 = new SqlCommand("UPDATE Joc SET Data_sfarsit_joc=@data_s,Id_invingator=@id_i WHERE Id_joc=@id ", sqlConn);
+                    cmd2.Parameters.AddWithValue("@data_s", DateTime.Now);
+                    cmd2.Parameters.AddWithValue("@id_i", id_invingator.ToString());
+                    cmd2.Parameters.AddWithValue("@id", id_joc.ToString());
+                    cmd2.ExecuteNonQuery();
+                    winner_lbl.Text = "Jocul s-a terminat! " + Convert.ToString(Label1.Text) + " a castigat!";
+                }
+
             }
         }
     }
